fc8a5a4134ea966d4226858c8e4a411d2679cfd2
team474 <team@polypost.io>
2025-10-11 02:44:27 -0400

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,7 +13,7 @@ export default async function BlogPage() {
       <div className="relative flex flex-col items-center gap-6 md:gap-12">
         <div className="flex max-w-[1170px] mx-auto flex-col justify-center items-center gap-3 md:gap-5">
           <h2 className="text-[26px] md:text-5xl font-black leading-[120%] text-[#101010] uppercase text-center font-playfair-display-sc">
-            All Blog
+            All Blogs
           </h2>
         </div>
         <Icon.leafIcon className="hidden md:flex absolute top-0 right-5 size-13" />
@@ -23,3 +23,4 @@ export default async function BlogPage() {
     </CompanyPage>
   );
 }
+
